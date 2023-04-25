9eaa754526ef7542c3f127cd46e8b54d20bd077c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2023-04-25 05:15:23 +0500

    new version of code with major changes

--- server.mjs
+++ server.mjs
@@ -1,8 +1,9 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import { WebhookClient } from 'dialogflow-fulfillment';
-import stripe from 'stripe';
+import Stripe from 'stripe';
+
+const stripe = new Stripe('your_stripe_secret_key', { apiVersion: '2020-08-27' });
 
 const app = express()
 app.use(express.json())
@@ -32,7 +33,37 @@ const intentResponses = {
             }
         ]
     },
-    "HandlePayment": handlePayment,
+    "HandlePayment": {
+        "fulfillmentMessages": [
+            {
+                "payload": {
+                    "richContent": [
+                        [
+                            {
+                                "type": "info",
+                                "title": "Payment Information",
+                                "subtitle": "Please fill in the details below to proceed with the payment",
+                                "image": {
+                                    "src": {
+                                        "rawUrl": "https://www.gstatic.com/dialogflow/images/branding/dialogflow_logo_128dp.png"
+                                    }
+                                }
+                            }
+                        ],
+                        [
+                            {
+                                "type": "custom",
+                                "webview": {
+                                    "height": "tall",
+                                    "src": "https://yourwebpage.com/paymentform"
+                                }
+                            }
+                        ]
+                    ]
+                }
+            }
+        ]
+    },
     default: {
         fulfillmentMessages: [
             {
@@ -48,10 +79,10 @@ const intentResponses = {
 
 app.post('/webhook', async (req, res) => {
     try {
-        const agent = new WebhookClient({ request: req, response: res });
-        const intentName = agent.intent.displayName;
-        const intentHandler = intentResponses[intentName] || intentResponses.default;
-        await intentHandler(agent);
+        const { queryResult } = req.body;
+        const intentName = queryResult.intent.displayName;
+        const response = intentResponses[intentName] || intentResponses.default;
+        res.send({ fulfillmentMessages: response.fulfillmentMessages });
     } catch (err) {
         console.log(err);
         res.send({
@@ -59,7 +90,7 @@ app.post('/webhook', async (req, res) => {
                 {
                     "text": {
                         "text": [
-                            "Something went wrong on our server. Please try again later."
+                            "something is wrong in server, please try again"
                         ]
                     }
                 }
@@ -68,33 +99,139 @@ app.post('/webhook', async (req, res) => {
     }
 });
 
-/*---------------------Static Files--------------------------*/
+/*---------------------Payment Form Handler--------------------------*/
 
-const __dirname = path.resolve();
-app.get('/', express.static(path.join(__dirname, "/Web/index.html")));
-app.use('/', express.static(path.join(__dirname, "/Web")));
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-/*---------------------Handle Payment--------------------------*/
-
-async function handlePayment(agent) {
-    const stripe_secret_key = "YOUR_STRIPE_SECRET_KEY";
-    const { amount, token } = agent.parameters;
-    const stripeClient = new stripe(stripe_secret_key);
+app.post('/process_payment', async (req, res) => {
     try {
-        const charge = await stripeClient.charges.create({
-            amount: amount * 100,
-            currency: "usd",
-            source: token.id,
-            description: "Payment from Dialogflow"
+        const { name, email, card_number, amount } = req.body;
+
+        // Validate the form fields
+        if (!name || !email || !card_number || !amount) {
+            return res.send({
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                            "text": [
+                                "Please fill in all the fields to proceed with the payment."
+                            ]
+                        }
+                    }
+                ]
+            });
+        }
+
+        if (!Stripe.paymentMethods.card.validateCardNumber(card_number)) {
+            return res.send({
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                            "text": [
+                                "Please enter a valid card number."
+                            ]
+                        }
+                    }
+                ]
+            });
+        }
+
+        if (!Stripe.paymentMethods.card.validateExpiryDate(month, year)) {
+            return res.send({
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                            "text": [
+                                "Please enter a valid expiry date."
+                            ]
+                        }
+                    }
+                ]
+            });
+        }
+
+        const paymentMethod = await stripe.paymentMethods.create({
+            type: 'card',
+            card: {
+                number: card_number,
+                exp_month: month,
+                exp_year: year,
+            },
+            billing_details: {
+                name: name,
+                email: email
+            }
         });
-        console.log("charge", charge);
-        agent.add(`Your payment of ${amount} USD was successful. Thank you!`);
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount * 100,
+            currency: 'usd',
+            payment_method: paymentMethod.id,
+            confirmation_method: 'manual',
+            confirm: true
+        });
+
+        if (paymentIntent.status === 'requires_action') {
+            const { error } = await stripe.confirmCardPayment(
+                paymentIntent.client_secret
+            );
+            if (error) {
+                console.log('Payment failed:', error);
+                res.send({
+                    "fulfillmentMessages": [
+                        {
+                            "text": {
+                                "text": [
+                                    "Payment failed. Please try again later."
+                                ]
+                            }
+                        }
+                    ]
+                });
+            } else {
+                console.log('Payment succeeded:', paymentIntent.id);
+                res.send({
+                    "fulfillmentMessages": [
+                        {
+                            "text": {
+                                "text": [
+                                    "Payment successful. Your payment ID is: " + paymentIntent.id
+                                ]
+                            }
+                        }
+                    ]
+                });
+            }
+        } else {
+            console.log('Payment succeeded:', paymentIntent.id);
+            res.send({
+                "fulfillmentMessages": [
+                    {
+                        "text": {
+                            "text": [
+                                "Payment successful. Your payment ID is: " + paymentIntent.id
+                            ]
+                        }
+                    }
+                ]
+            });
+        }
     } catch (err) {
         console.log(err);
-        agent.add("Sorry, there was an error processing your payment. Please try again later.");
+        res.send({
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [
+                            "Payment failed. Please try again later."
+                        ]
+                    }
+                }
+            ]
+        });
     }
-}
+});
+
+/*---------------------Listen to App--------------------------*/
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
